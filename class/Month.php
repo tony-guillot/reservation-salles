e4c5bba96f137e1d9270f206c5d1013d3bb142d6
tony-guillot <tony.guilot@laplateforme.io>
2022-01-19 19:05:23 +0100

    calendirer avec date et jour + boutton mois suivant et precedent

--- class/Month.php
+++ class/Month.php
@@ -3,8 +3,9 @@
 class Month{
     
     private $months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout','Septembre','Octobre', 'Novembre', 'Decembre'];
-    private $month;
-    private $years;
+    public $days = ['Lundi', 'Mardi', 'Mercredi','Jeudi','Vendredi','Samedi', 'Dimanche'];
+    public  $month;
+    public  $years;
 
     /**
      * @param int $month mois compris entre 1  et 12
@@ -15,7 +16,7 @@ class Month{
 
         if($month === null){
 
-            $month = intVal(date('m'));
+            $month = intVal(date('m')); // si le mois est null , month = mois en cours : intavl = retourne un int à la place d'une string
         }
 
         if($years === null){
@@ -24,41 +25,103 @@ class Month{
         }
         if($month < 1 || $month > 12){
 
-            throw new Exception("Le mois n'est pas valide");
-        }
-        if($years <  1970){
-
-            throw new Exception("L'année est inférieur à 1970");
-
+            throw new Exception("Le mois n'est pas valide"); // créer l'exeption a afficher que l'on récupère apres avec un try et un catch 
         }
 
         $this->month = $month;
         $this->years = $years;
+        
     }
 
+    /**
+     * @return \DateTime renvoie le premieyr jour du mois 
+     */
+    public function getFirstDay(): \DateTime{
+
+        return new \Datetime("{$this->years}-{$this->month}-01"); 
+
+
+    }
  
 
     
     /** retourn le mois en toute lettre 
      * @return string
      */
+    
     public function toString(): string{
 
         return $this->months[$this->month - 1]. ' '.$this->years;
+        
     }
     
-    public function getWeeks(): int{
+    /**
+     * @return int permet de recuperer le nombre de semaine dans le mois en un entier 
+     */
+    
+     public function getWeeks(): int{
 
-        $start = new \Datetime("{$this->years}-{$this->month}-01");
-        $end = (clone $start)->modify('+1 month -1 day');
+        $start = $this->getFirstDay(); 
+        $end = (clone $start)->modify('+1 month -1 day'); // clone la variable $start pour pas la modifier et ajoute un mois et un jour pour arriver a la fin du mois en cours
 
         $weeks =  intval($end->format('W')) - intVal($start->format('W')) +1;
         if($weeks < 0){
 
-            $weeks = intval($end->format('W'));
+            $weeks = intval($end->format('W')); // format retourne la date formaté au format voulu , ici W = jours de la semaine au fromat numérique
 
             
         }
         return $weeks;
+    }
+
+    /**
+     * est-ce que le jour est dans le mois en cours
+     * @param DateTime $date
+     * 
+     * @return bool
+     */
+    
+    
+    public function WithinMonth( \DateTime $date): bool {
+
+        return $this->getFirstDay()->format('Y-m') === $date->format('Y-m');
+
+    }
+
+    /**
+     * Renvoi le mois nuivant
+     * @return month
+     */
+    public function nextMonth(): month{
+
+        $month = $this->month +1;
+        $years = $this->years;
+       
+        if($month > 12){
+
+            $month = 1;
+            $years = $years++;
+            
+
+        } 
+        return new Month($month, $years);
+    }
+
+   
+    /**
+     * renvoie le mois precedent
+     * @return month
+     */
+    public function previousMonth(): month{
+
+        $month = $this->month - 1;
+        $years = $this->years;
+
+        if($month < 1){
+
+            $month = 12;
+            $years -= 1;
+        }
+        return new Month($month,$years);
     }
 }
--- planning.php
+++ planning.php
@@ -8,6 +8,11 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="include/calendar.css">
+
+    
+
     <title>Document</title>
 </head>
 <body>
@@ -22,33 +27,57 @@ session_start();
     try{
         
         $month = new Month($_GET['month'] ?? null, $_GET['years '] ?? null);
+        $start = $month->getFirstDay()->modify('last monday');
     
     }catch(\Exception $e){
 
         $month  = new Month();
     }
+    
+    ?>  
+        
+        <div class="calendar_btn">
+        <p id="calendar_p">Planning des reservations des salles</p>
+        <h1 ><?= $month->toString()?></h1>
+            <div>
+                <a href="planning.php?month=<?= $month->previousMonth()->month; ?>&years=<?= $month->previousMonth()->years; ?>" class="btn btn-primary">&lt;</a>
 
-    ?>
-    <?= $month->getWeeks();?>
-    <table>
+                <a href="planning.php?month=<?= $month->nextMonth()->month; ?>&years=<?= $month->nextMonth()->years;?>" class="btn btn-primary">&gt;</a>
+
+            </div>
+        </div>
+
+        
+   
 
     
 
-     <?php  for($i=0; $i < $month->getWeeks(); $i++):?> 
+  
+    
 
+    <table class="calendar_table calendar_table--<?=$month->getWeeks();?>weeks">
+
+     <?php  for($i=0; $i < $month->getWeeks(); $i++):?> 
+        <!-- boucle les jour de la semaine selon le nombres de semaines dans le mois  -->
      <tr>
-         <td>Lundi</td>
-         <td>Mardi</td>
-         <td>Mercredi</td>
-         <td>Jeudi</td>
-         <td>Vendredi</td>
-         <td>Samedi</td>
-         <td>Dimanche</td>
+         <?php  foreach ($month->days as $k => $day): 
+            
+           $date = (clone $start)->modify("+". ($k + $i *7). "days")  ;?>  
+            
+            <!-- $k stock la valeur du jour -->
+         <td class ="<?= $month->WithinMonth($date) ? ' ' : 'calendar_othermonth' ?>">  <!-- si c'est vrais on met rien sinon la class calendar_othermonth -->
+
+           <?php if($i === 0): ?>
+            <div class="calendar_weekday"> <?= $day ;?></div>
+            <?php endif; ?>
+           <div class="calendar_day"><?= $date->format('d');?> </div>
+         </td>
+         <?php endforeach; ?>
      </tr>
-        <?php endfor; ?> <!-- enfore remplace les accolade php  -->
+        <?php endfor; ?> <!-- endfor remplace les accolade php  -->
     </table>
     
 
-    <h1><?= $month->toString()?></h1>
+    
 </body>
 </html>
